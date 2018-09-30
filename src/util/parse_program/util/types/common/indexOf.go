96b1b59dbe43a91918b6e9a6009c6c361277785f
brad medeiros <bradmedeiros0@gmail.com>
2018-09-29 23:09:08 -0700

    remove index of

--- src/util/parse_program/util/types/common/indexOf.go
+++
@@ -1,9 +0,0 @@
-package common
-import "fmt"
-
-func IndexOf(stringToLookIn string, stringToSearchFor string) int {
-	for i:= 0; i < len(stringToLookIn); i++ {
-		fmt.Println(stringToLookIn[i])
-	}
-	return 0
-}
